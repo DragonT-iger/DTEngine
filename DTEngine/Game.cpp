023a9f78f974c45fb63539c4730b64149f141df1
kkancho87 <snow021101@naver.com>
2026-02-02 19:13:12 +0900

    Update Game.cpp

--- DTEngine/Game.cpp
+++ DTEngine/Game.cpp
@@ -105,8 +105,8 @@ bool Game::Initialize()
 
 
 
-	//SceneManager::Instance().RegisterScene("Scenes/GridTestScene.scene");
-	//SceneManager::Instance().LoadScene("GridTestScene");
+	SceneManager::Instance().RegisterScene("Scenes/GridTestScene.scene");
+	SceneManager::Instance().LoadScene("GridTestScene");
 	//SceneManager::Instance().RegisterScene("Scenes/DTtestScene.scene");
 	//SceneManager::Instance().LoadScene("DTtestScene");
 
@@ -117,8 +117,8 @@ bool Game::Initialize()
 	 //SceneManager::Instance().RegisterScene("Scenes/SampleSceneBum.scene");
 	 //SceneManager::Instance().LoadScene("SampleSceneBum");
 
-	SceneManager::Instance().RegisterScene("Scenes/SampleScene.scene");
-	SceneManager::Instance().LoadScene("SampleScene");
+	//SceneManager::Instance().RegisterScene("Scenes/SampleScene.scene");
+	//SceneManager::Instance().LoadScene("SampleScene");
 
 	/* SceneManager::Instance().RegisterScene("Scenes/DTtestScene.scene");
 	 SceneManager::Instance().LoadScene("DTtestScene");*/
