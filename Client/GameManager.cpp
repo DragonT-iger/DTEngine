31bf26ffb4aac6205b8b1bcae40868bd1a796fa4
Seoki2000 <u826194@gmail.com>
2026-02-05 15:09:10 +0900

    gamemanager text 제거.

--- Client/GameManager.cpp
+++ Client/GameManager.cpp
@@ -7,8 +7,8 @@
 
 BEGINPROPERTY(GameManager)
 //DTPROPERTY(GameManager, m_leftHealth) static inline은 직렬화 시 의미가 없어짐 값이 덮힘 주의할것
-DTPROPERTY(GameManager, m_money)
-DTPROPERTY(GameManager, m_life)
+//DTPROPERTY(GameManager, m_money)
+//DTPROPERTY(GameManager, m_life)
 ENDPROPERTY()
 
 
