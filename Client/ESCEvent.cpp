eb62e14c9eb63f3035f6b4afa75e5983f34bf142
Seoki2000 <u826194@gmail.com>
2026-02-06 15:18:30 +0900

    esc event 추가. scene에 설정 event 추가.

---
+++ Client/ESCEvent.cpp
@@ -0,0 +1,50 @@
+#include "ESCEvent.h"
+#include "GameObject.h"
+#include "UIButton.h"
+#include "GameManager.h"
+#include "InputManager.h"
+
+BEGINPROPERTY(ESCEvent)
+DTPROPERTY(ESCEvent, m_settingButton)
+DTPROPERTY(ESCEvent, m_settingWindow)
+DTPROPERTY(ESCEvent, m_window)
+ENDPROPERTY()
+
+
+void ESCEvent::Update(float deltaTime)
+{
+		if (InputManager::Instance().GetKeyDown(KeyCode::Escape))
+		{
+				Check();
+		}
+}
+
+void ESCEvent::Check()
+{
+		if (m_window)
+		{
+				// 있으면 무조건 닫기.
+				m_window->SetActive(false);
+		}
+
+		if (!m_settingWindow)
+				return;
+
+		bool isActive = m_settingWindow->IsActive();
+
+		// 이전 배속 가져오기.
+		int timeScale = GameManager::Instance()->GetPrevTimeScale();
+
+		// 토글.
+		m_settingWindow->SetActive(!isActive);
+
+		// 켜지면 0 아니면 원래 배속.
+		if (!isActive)
+		{
+				GameManager::Instance()->SetTimeScale(0);
+		}
+		else
+		{
+				GameManager::Instance()->SetTimeScale(timeScale);
+		}
+}
