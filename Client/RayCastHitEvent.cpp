b5c04cb95ee7b913154fbe4b7dc8a38996975a7b
kkancho87 <snow021101@naver.com>
2026-02-11 23:37:59 +0900

    화살표 시작부분 걍 날림. 고양이 소리 추가

--- Client/RayCastHitEvent.cpp
+++ Client/RayCastHitEvent.cpp
@@ -169,12 +169,12 @@ void RayCastHitEvent::RaycastCheck()
 									tr->SetRotationEuler(e);
 									heads[i]->SetActive(true);
 
-									tr = starts[i]->GetTransform();
-									tr->SetPosition(path[i].startWorld);
-									e = tr->GetEditorEuler();
-									e.y = path[i].yawDeg;
-									tr->SetRotationEuler(e);
-									starts[i]->SetActive(true);
+									//tr = starts[i]->GetTransform();
+									//tr->SetPosition(path[i].startWorld);
+									//e = tr->GetEditorEuler();
+									//e.y = path[i].yawDeg;
+									//tr->SetRotationEuler(e);
+									//starts[i]->SetActive(true);
 								}
 								m_arrowPool->_GetOwner()->SetActive(true);
 
@@ -379,12 +379,12 @@ void RayCastHitEvent::RaycastCheck()
 								tr->SetRotationEuler(e);
 								heads[i]->SetActive(true);
 
-								tr = starts[i]->GetTransform();
-								tr->SetPosition(path[i].startWorld);
-								e = tr->GetEditorEuler();
-								e.y = path[i].yawDeg;
-								tr->SetRotationEuler(e);
-								starts[i]->SetActive(true);
+								//tr = starts[i]->GetTransform();
+								//tr->SetPosition(path[i].startWorld);
+								//e = tr->GetEditorEuler();
+								//e.y = path[i].yawDeg;
+								//tr->SetRotationEuler(e);
+								//starts[i]->SetActive(true);
 							}
 
 							m_arrowPool->_GetOwner()->SetActive(true);
